1b0836a2cc961a18fcbce330e1d56dad9fe1fafd
Gustavo López <gualopezb@gmail.com>
2020-12-31 10:27:45 -0500

    Load dev db from env vars

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :codexamer, Codexamer.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USERNAME"),
+  password: System.get_env("POSTGRES_PASSWORD"),
   database: "codexamer_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
